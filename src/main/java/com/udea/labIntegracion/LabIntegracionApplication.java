a4bc22f2237e9c3521f7af7cd73769c8fa8bfe07
andre <andrea.marind.1@gmail.com>
2025-10-14 17:05:50 -0500

    Repo base

---
+++ src/main/java/com/udea/labIntegracion/LabIntegracionApplication.java
@@ -0,0 +1,13 @@
+package com.udea.labIntegracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabIntegracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabIntegracionApplication.class, args);
+	}
+
+}
